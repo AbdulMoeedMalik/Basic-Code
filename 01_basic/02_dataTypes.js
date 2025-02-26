b0157dceb3ef2b25dbcaea13f8393d35fb0f4b01
Malik Moeed <amoeed923@gmail.com>
2025-02-26 10:16:34 +0000

    Data Types

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -2,16 +2,21 @@
 
 // alert("Hello") 
 
+// number => 2 to power 53
+// bigInt large number
+// string => "" ''
+// boolean => true/fasle 
+// undefined =>  unsatandalone value
+// symbols  => unique
+//  Object
+
+
 let name = "moeed" // String 
 let age = 12 // Number
 let isLoggedIn = false // Bollean
 let number =  203944959660 //   bigint use in large number 
 let state = null // standalone Valuse 
-// undefined =>  unsatandalone value
-// symbols  => unique
 
-
-//Object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object 
