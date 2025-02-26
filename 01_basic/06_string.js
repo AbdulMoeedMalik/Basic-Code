0a6b64e0a0ec0413d1044e6896d33e2fb975262f
Malik Moeed <amoeed923@gmail.com>
2025-02-26 12:04:02 +0000

    String

--- 01_basic/06_string.js
+++ 01_basic/06_string.js
@@ -11,26 +11,51 @@ const gameName = new String('Moeed-hc-com')
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+//  console.log(gameName.length);
+//  console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.indexOf('c'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 5)
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-4, 10)
+// console.log(anotherString);
 
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+  // trim Method
+
+const greeting = "   Hello world!  ";
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// Expected output: "Hello world!";
+
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('moeed'))
 
 console.log(gameName.split('-'));
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(words[3]);
+// Expected output: "fox"
+
+const chars = str.split("");
+console.log(chars[8]);
+// Expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
+
 
