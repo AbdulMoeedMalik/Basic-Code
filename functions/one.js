65f0a06613cab0c1c40ddaa5ac82ed13d54730ac
Malik Moeed <amoeed923@gmail.com>
2025-02-28 13:18:28 +0000

    One

--- functions/one.js
+++ functions/one.js
@@ -28,4 +28,21 @@
  printMeAgain( 20 ,30);
 
 
+
+ /**
+  *What is this?
+
+    In JavaScript, the this keyword refers to an object.
+
+    The this keyword refers to different objects depending on how it is used:
+
+    In an object method, this refers to the object.
+    Alone, this refers to the global object.
+    In a function, this refers to the global object.
+    In a function, in strict mode, this is undefined.
+    In an event, this refers to the element that received the event.
+    Methods like call(), apply(), and bind() can refer this to any object.
+  */
+
+
  
