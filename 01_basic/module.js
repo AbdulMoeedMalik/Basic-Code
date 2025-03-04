6d8a8f557944696fec13ae62e68be6c9a1dbe257
Malik Moeed <amoeed923@gmail.com>
2025-03-04 15:55:33 +0000

    Modules

---
+++ 01_basic/module.js
@@ -0,0 +1,19 @@
+// ES6 Modules
+    export const hello = () =>{
+    console.log("Hello Harry");
+    
+}
+export const ahello = (name) =>{
+    console.log("Hello " + name );
+    
+}
+
+   const zain = () =>{
+    console.log("Hello " + "Zain" );
+    
+}
+
+export default zain;
+
+
+
---
+++ 01_basic/modules1.js
@@ -0,0 +1,7 @@
+// const {hello , ahello} = require("./module.js") // Simple Module 
+import harry , {hello ,ahello} from "./module.js"
+hello()
+ahello("Moeed")
+ahello("Ali")
+ahello("Malik")
+harry()
