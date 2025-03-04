9418e4bfabc65b0fbc0c0f3596d3dedb3429db76
Malik Moeed <amoeed923@gmail.com>
2025-03-04 20:25:07 +0000

    Arrays

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -39,14 +39,32 @@ myArr.unshift(9)
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+// const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
+
+const num = [ 2 , 35 , 43 , 46 , 50 , 60 ]
+// num.splice(4,0,100)
+// console.log(num);
+
+//  num.splice(4,0,100)
+//  console.log(num);
+ 
+num.splice(3,0,100)
+ console.log(num);
+ 
+
+
+
+ 
+ 
+ 
+ 
